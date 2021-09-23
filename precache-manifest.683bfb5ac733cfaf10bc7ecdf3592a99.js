f0e13ef5eec9e09706ece0e96c85f27d580603f9
shaffanFast <k190863@nu.edu.pk>
2021-09-22 22:28:12 -0700

    Updates

---
+++ precache-manifest.683bfb5ac733cfaf10bc7ecdf3592a99.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a42fb0b2cbe3020f522",
+    "url": "/iParhaiWordProblems/static/css/main.68c1005d.chunk.css"
+  },
+  {
+    "revision": "1a42fb0b2cbe3020f522",
+    "url": "/iParhaiWordProblems/static/js/main.8e5dd811.chunk.js"
+  },
+  {
+    "revision": "c13c997b803bed682b54",
+    "url": "/iParhaiWordProblems/static/js/runtime~main.c680fe07.js"
+  },
+  {
+    "revision": "16b4170b66758af59760",
+    "url": "/iParhaiWordProblems/static/js/2.0659f8e9.chunk.js"
+  },
+  {
+    "revision": "2c47ff86e35f8a2c75be",
+    "url": "/iParhaiWordProblems/static/js/3.b1e1859a.chunk.js"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/iParhaiWordProblems/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/iParhaiWordProblems/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/iParhaiWordProblems/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/iParhaiWordProblems/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/iParhaiWordProblems/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/iParhaiWordProblems/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/iParhaiWordProblems/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/iParhaiWordProblems/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/iParhaiWordProblems/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/iParhaiWordProblems/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/iParhaiWordProblems/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/iParhaiWordProblems/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/iParhaiWordProblems/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/iParhaiWordProblems/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "454ad559d942984a09969f8333f7aae4",
+    "url": "/iParhaiWordProblems/index.html"
+  }
+];
